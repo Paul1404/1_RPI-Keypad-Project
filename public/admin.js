a4c184d06d2345dd24d541a0b8362c2debcfe72b
Paul <97813342+Paul1404@users.noreply.github.com>
2023-10-24 09:46:29 +0200

    Forgor to push these changes

--- public/admin.js
+++ public/admin.js
@@ -1,30 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Define constants for messages
+  const LOGIN_SUCCESSFUL = 'Login successful';
+  const INVALID_CREDENTIALS = 'Invalid credentials';
+  
   const form = document.getElementById('adminLoginForm');
   
-  form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', async function(event) {
     event.preventDefault();
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     
-    fetch('/admin-login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.message === 'Login successful') {
-        // Redirect to admin dashboard
-        window.location.href = '/admin_dashboard';
+    try {
+      const data = await loginUser(username, password);
+      
+      if (data.message === LOGIN_SUCCESSFUL) {
+        redirectToAdminDashboard();
       } else {
-        alert('Invalid credentials');
+        showAlert(INVALID_CREDENTIALS);
       }
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error('Error:', error);
-    });
+    }
   });
 });
+
+async function loginUser(username, password) {
+  const response = await fetch('/admin-login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
+  }
+
+  return response.json();
+}
+
+function redirectToAdminDashboard() {
+  window.location.href = '/admin_dashboard';
+}
+
+function showAlert(message) {
+  alert(message);
+}
