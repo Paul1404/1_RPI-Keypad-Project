6b13769a7fe72c40e6e3a9e5f7d2a6d1f12f0b91
Paul <97813342+Paul1404@users.noreply.github.com>
2023-10-27 22:34:49 +0200

    Documentation Update for admin.js (JSDoc Conform)

--- public/admin.js
+++ public/admin.js
@@ -1,19 +1,28 @@
+/**
+ * Event listener for the DOMContentLoaded event.
+ * Initializes the form submit event listener.
+ */
 document.addEventListener("DOMContentLoaded", function() {
   // Define constants for messages
   const LOGIN_SUCCESSFUL = 'Login successful';
   const INVALID_CREDENTIALS = 'Invalid credentials';
-  
+
   const form = document.getElementById('adminLoginForm');
-  
+
+  /**
+   * Event listener for the form submit event.
+   * Authenticates the admin and redirects on success.
+   * @param {Event} event - The DOM event object
+   */
   form.addEventListener('submit', async function(event) {
     event.preventDefault();
-    
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    
+
     try {
       const data = await loginUser(username, password);
-      
+
       if (data.message === LOGIN_SUCCESSFUL) {
         redirectToAdminDashboard();
       } else {
@@ -25,6 +34,13 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+/**
+ * Asynchronously login the admin user.
+ * @async
+ * @param {string} username - The admin username
+ * @param {string} password - The admin password
+ * @returns {Promise<Object>} The server response as a JSON object
+ */
 async function loginUser(username, password) {
   const response = await fetch('/admin-login', {
     method: 'POST',
@@ -41,10 +57,17 @@ async function loginUser(username, password) {
   return response.json();
 }
 
+/**
+ * Redirects the user to the admin dashboard.
+ */
 function redirectToAdminDashboard() {
   window.location.href = '/admin_dashboard';
 }
 
+/**
+ * Shows an alert with a specified message.
+ * @param {string} message - The message to display
+ */
 function showAlert(message) {
   alert(message);
 }
