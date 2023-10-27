81803b589ac96ee5b522d3fe31db8bcfc23d4205
Paul <97813342+Paul1404@users.noreply.github.com>
2023-10-27 22:37:59 +0200

    Documentation Update for admin_dashboard.js (JSDoc Conform)

--- public/admin_dashboard.js
+++ public/admin_dashboard.js
@@ -1,27 +1,30 @@
+/**
+ * Event listener for DOMContentLoaded event.
+ * It fetches the admin dashboard content and initializes form submit listeners.
+ */
 document.addEventListener("DOMContentLoaded", function () {
-  // Fetch admin dashboard content upon page load
+  // Fetch admin dashboard content and handle unauthorized access.
   fetch('/admin_dashboard')
-  .then(response => {
-    if (response.status === 401) {
-      // Handle unauthorized access, possibly redirect to login page
-      window.location.href = "/admin.html";
-      return;
-    }
-    return response.json();
-  })
-  .then(data => {
-    if (data) {
-      // Populate the admin dashboard using the received data
-    }
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+      .then(response => {
+        if (response.status === 401) {
+          window.location.href = "/admin.html";
+          return;
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data) {
+          // Populate the admin dashboard
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
 
+  // Event listener for add PIN form submit.
   const form = document.getElementById("addPinForm");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-
     const newPin = document.getElementById("newPin").value;
 
     fetch("/add-pin", {
@@ -31,24 +34,24 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       body: JSON.stringify({ pin: newPin }),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.message === "PIN added successfully") {
-        alert("PIN added successfully");
-      } else {
-        alert("Failed to add PIN");
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.message === "PIN added successfully") {
+            alert("PIN added successfully");
+          } else {
+            alert("Failed to add PIN");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
   });
 });
 
+// Event listener for remove PIN form submit.
 const removePinForm = document.getElementById("removePinForm");
 removePinForm.addEventListener("submit", function (event) {
   event.preventDefault();
-
   const removePin = document.getElementById("removePin").value;
 
   fetch("/remove-pin", {
@@ -58,31 +61,31 @@ removePinForm.addEventListener("submit", function (event) {
     },
     body: JSON.stringify({ pin: removePin }),
   })
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.message === "PIN removed successfully") {
-      alert("PIN removed successfully");
-    } else {
-      alert("Failed to remove PIN");
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "PIN removed successfully") {
+          alert("PIN removed successfully");
+        } else {
+          alert("Failed to remove PIN");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 });
 
+// Event listener for "Go Back" button click.
 const goBackButton = document.getElementById("goBackButton");
 goBackButton.addEventListener("click", function () {
   window.location.href = "/";
 });
 
-
+// Event listener for add admin form submit.
 const addAdminForm = document.getElementById("addAdminForm");
 addAdminForm.addEventListener("submit", function (event) {
   event.preventDefault();
-
   const adminUsername = document.getElementById("newAdmin").value;
-  const adminPassword = document.getElementById("newAdminPassword").value;  
+  const adminPassword = document.getElementById("newAdminPassword").value;
 
   fetch("/add-admin", {
     method: "POST",
@@ -91,23 +94,23 @@ addAdminForm.addEventListener("submit", function (event) {
     },
     body: JSON.stringify({ username: adminUsername, password: adminPassword }),
   })
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.message === "Admin added successfully") {
-      alert("Admin added successfully");
-    } else {
-      alert("Failed to add admin");
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Admin added successfully") {
+          alert("Admin added successfully");
+        } else {
+          alert("Failed to add admin");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 });
 
+// Event listener for remove admin form submit.
 const removeAdminForm = document.getElementById("removeAdminForm");
 removeAdminForm.addEventListener("submit", function (event) {
   event.preventDefault();
-
   const adminUsername = document.getElementById("removeAdmin").value;
 
   fetch("/remove-admin", {
@@ -117,15 +120,15 @@ removeAdminForm.addEventListener("submit", function (event) {
     },
     body: JSON.stringify({ username: adminUsername }),
   })
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.message === "Admin removed successfully") {
-      alert("Admin removed successfully");
-    } else {
-      alert("Failed to remove admin");
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Admin removed successfully") {
+          alert("Admin removed successfully");
+        } else {
+          alert("Failed to remove admin");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 });
